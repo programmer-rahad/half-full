8f2cbff3198c2dba313e1e702efa7d38f32f66f9
programmer-rahad <programmer.rahad@gmail.com>
2023-11-11 21:04:44 +0600

    Half-Full

---
+++ assets/js/index.js
@@ -0,0 +1,100 @@
+!function () {
+    // event: DOM Content Loaded
+
+    function allLoaded() {
+
+        // Header
+
+        !function () {
+            const toggleBtn = document.querySelector('.toggle-bar');
+            toggleBtn.addEventListener('click', function () {
+                const nav = this.nextElementSibling;
+                nav.classList.toggle('active');
+                let height = nav.children[0].offsetHeight + nav.children[1].offsetHeight;
+                if (nav.classList.contains('active')) {
+                    nav.style.height = height + 'px';
+                    this.innerHTML = '<i class="fas fa-times"></i>';
+                } else {
+                    nav.style.height = 0;
+                    this.innerHTML = '<i class="fas fa-bars"></i>';
+                }
+            })
+        }();
+
+        !function () {
+            var x, i, j, l, ll, selElmnt, a, b, c;
+            x = document.getElementsByClassName("custom-select");
+            l = x.length;
+            for (i = 0; i < l; i++) {
+                selElmnt = x[i].getElementsByTagName("select")[0];
+                ll = selElmnt.length;
+                a = document.createElement("DIV");
+                a.setAttribute("class", "select-selected");
+                a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+                x[i].appendChild(a);
+                b = document.createElement("DIV");
+                b.setAttribute("class", "select-items select-hide");
+                for (j = 1; j < ll; j++) {
+                    c = document.createElement("DIV");
+                    c.innerHTML = selElmnt.options[j].innerHTML;
+                    c.addEventListener("click", function (e) {
+                        var y, i, k, s, h, sl, yl;
+                        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                        sl = s.length;
+                        h = this.parentNode.previousSibling;
+                        for (i = 0; i < sl; i++) {
+                            if (s.options[i].innerHTML == this.innerHTML) {
+                                s.selectedIndex = i;
+                                h.innerHTML = this.innerHTML;
+                                y = this.parentNode.getElementsByClassName("same-as-selected");
+                                yl = y.length;
+                                for (k = 0; k < yl; k++) {
+                                    y[k].removeAttribute("class");
+                                }
+                                this.setAttribute("class", "same-as-selected");
+                                break;
+                            }
+                        }
+                        h.click();
+                    });
+                    b.appendChild(c);
+                }
+                x[i].appendChild(b);
+                a.addEventListener("click", function (e) {
+                    e.stopPropagation();
+                    closeAllSelect(this);
+                    this.nextSibling.classList.toggle("select-hide");
+                    this.classList.toggle("select-arrow-active");
+                });
+            }
+            function closeAllSelect(elmnt) {
+                var x, y, i, xl, yl, arrNo = [];
+                x = document.getElementsByClassName("select-items");
+                y = document.getElementsByClassName("select-selected");
+                xl = x.length;
+                yl = y.length;
+                for (i = 0; i < yl; i++) {
+                    if (elmnt == y[i]) {
+                        arrNo.push(i)
+                    } else {
+                        y[i].classList.remove("select-arrow-active");
+                    }
+                }
+                for (i = 0; i < xl; i++) {
+                    if (arrNo.indexOf(i)) {
+                        x[i].classList.add("select-hide");
+                    }
+                }
+            }
+            document.addEventListener("click", closeAllSelect);
+        }();
+
+
+
+
+
+
+    }
+
+    document.addEventListener('DOMContentLoaded', allLoaded)
+}();
